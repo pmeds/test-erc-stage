3ab70218a29c07c37400e5f23d8342e1d0233395
pmeds <109703314+pmeds@users.noreply.github.com>
2022-07-21 10:36:52 -0600

    Update check_old.sh

--- check_old.sh
+++ check_old.sh
@@ -4,8 +4,11 @@ FILES=$(echo "$i")
 
 if [[ $FILES =~ .*CSV.*|.*upload.*|.*rules.* ]]
 then
+   echo "Found old files"
    echo "removing $FILES"
    rm -rf $FILES
+else
+   echo "No files old files found"
 fi
 
 done
